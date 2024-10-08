91bdbfaec8654c8811b6e759de6c130a2a9017f6
Halyna Bondar <galya.marusyak@gmail.com>
2024-10-08 13:24:48 +0200

    New components created

---
+++ app/about_us/OurValues.js
@@ -0,0 +1,11 @@
+import styles from './page.module.css';
+
+export const OurValues = () => {
+    // TASK - React 1 week 1
+    // Create the "Our Values" section
+    // Use the descriptions provided in /app/about_us/README.md
+    // Some inspiration ideas found in /data/inspiration_about_us
+    return (
+        <p> ADD OUR VALUES HERE </p>
+    );
+};
--- app/about_us/page.js
+++ app/about_us/page.js
@@ -1,42 +1,12 @@
 import styles from './page.module.css';
+import {OurPartners} from "@/app/about_us/OurPartners";
+import {OurCrew} from "@/app/about_us/OurCrew";
+import {OurValues} from "@/app/about_us/OurValues";
 
 // TASK - React 1 week 1
 // After you are finished with creating the page, move the OurValues, OurCrew, OurPartners components into their own files
 // OurValues.js, OurCrew.js, OurPartners.js should live in this folder
 // import and use the components from the newly created files
-
-const OurValues = () => {
-  // TASK - React 1 week 1
-  // Create the "Our Values" section
-  // Use the descriptions provided in /app/about_us/README.md
-  // Some inspiration ideas found in /data/inspiration_about_us
-  return (
-    <p> ADD OUR VALUES HERE </p>
-  );
-};
-
-const OurCrew = () => {
-  // TASK - React 1 week 1
-  // Create the "Our Crew section"
-  // Use the descriptions provided in /app/about_us/README.md
-  // Use the pictures from /public/crew
-  // Some inspiration ideas found in /data/inspiration_about_us
-  return (
-    <p> ADD OUR CREW HERE </p>
-  );
-}
-
-const OurPartners = () => {
-  // TASK - React 1 week 1
-  // Create the "Our Crew section"
-  // Use the descriptions provided in /app/about_us/README.md
-  // Use the pictures from /public/crew
-  // Some inspiration ideas found in /data/inspiration_about_us
-  return (
-    <p> ADD OUR Partners HERE </p>
-  );
-}
-
 
 export const Crew = () => {
   return (
@@ -53,7 +23,7 @@ export const Crew = () => {
         </section>
 
          {/* TASK - React 1 week 1 */}
-         {/* Add in the "OurPartners" component here */}
+         <OurPartners/>
       </main>
     </div>
   );
